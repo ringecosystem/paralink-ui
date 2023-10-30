25ac134e1e2f31cd0a7afb7e618afee85ac1d5d5
JayJay1024 <lygm1062@gmail.com>
2023-10-30 12:30:35 +0800

    Create header and footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <div className="app-footer">
+      <div className="container mx-auto flex h-full items-center justify-between px-middle">
+        <span>allright</span>
+        <span>github</span>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <div className="app-header fixed left-0 top-0 w-full">
+      <div className="container mx-auto flex h-full items-center justify-between px-middle">
+        <span>Logo</span>
+        <span>Connect Wallet</span>
+      </div>
+    </div>
+  );
+}
