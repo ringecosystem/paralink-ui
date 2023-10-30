a0bedfd054654913c07f85b5ec4b99e7dc7b386e
JayJay1024 <lygm1062@gmail.com>
2023-10-30 12:30:53 +0800

    Update root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import RainbowProvider from "@/providers/rainbow-provider";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,7 +16,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={`${inter.className} bg-app-black text-base font-normal text-white`}>
-        <RainbowProvider>{children}</RainbowProvider>
+        <RainbowProvider>
+          <Header />
+          {children}
+          <Footer />
+        </RainbowProvider>
       </body>
     </html>
   );
