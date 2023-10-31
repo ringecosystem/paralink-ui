59451edd86b8635f2a0bf55fc8719beb5bc73610
JayJay1024 <lygm1062@gmail.com>
2023-10-31 16:22:18 +0800

    New notification ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,7 @@ const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/ui/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
@@ -25,6 +26,43 @@ const config: Config = {
       screens: {
         xl: "1200px",
         "2xl": "1200px",
+      },
+      keyframes: {
+        rightenter: {
+          "0%": { opacity: "0", transform: "translateX(100%)" },
+          "1%": { opacity: "1" },
+          "100%": { transform: "translateX(0)" },
+        },
+        rightleave: {
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(100%)" },
+        },
+        notificationfadeout: {
+          "100%": { height: "0" },
+        },
+        countloadingsmall: {
+          "0%": {
+            height: "16px",
+          },
+          "50%, 100%": {
+            height: "8px",
+          },
+        },
+        countloadinglarge: {
+          "0%": {
+            height: "32px",
+          },
+          "50%, 100%": {
+            height: "16px",
+          },
+        },
+      },
+      animation: {
+        "notification-enter": "rightenter 400ms ease-out",
+        "notification-leave": "rightleave 400ms ease-out forwards",
+        "notification-fadeout": "notificationfadeout 200ms ease-out forwards",
+        "count-loading-small": "countloadingsmall 1200ms cubic-bezier(0, 0.5, 0.5, 1) infinite",
+        "count-loading-large": "countloadinglarge 1200ms cubic-bezier(0, 0.5, 0.5, 1) infinite",
       },
     },
   },
