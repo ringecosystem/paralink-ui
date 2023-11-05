6719117b023f8bf1c1886c0380e0ee5166f4f64a
JayJay1024 <lygm1062@gmail.com>
2023-11-06 05:50:48 +0800

    Improve ChainSelect

--- src/components/chain-select.tsx
+++ src/components/chain-select.tsx
@@ -15,23 +15,29 @@ export default function ChainSelect({ value, options, disabled, onChange = () =>
     <Select
       label={
         <div className="flex items-center gap-small">
-          <Image width={16} height={16} alt="Chain logo" src={getChainLogoSrc(value.logo)} />
-          <span>{value.name}</span>
+          <Image width={18} height={18} alt="Chain logo" src={getChainLogoSrc(value.logo)} />
+          <span className="text-white/50">{value.name}</span>
         </div>
       }
       disabled={disabled}
       sameWidth
-      childClassName="flex flex-col py-small"
+      labelClassName="flex items-center gap-middle py-[2px] px-[6px] bg-transparent hover:bg-white/10 border-radius"
+      childClassName="flex flex-col py-middle bg-component border-primary border border-radius"
+      arrowClassName="opacity-50"
     >
       {options?.length ? (
         options.map((chain) => (
-          <button key={chain.network} onClick={() => onChange(chain)} className="flex items-center gap-small">
+          <button
+            key={chain.network}
+            onClick={() => onChange(chain)}
+            className="flex items-center gap-small px-middle py-1 transition-colors hover:bg-white/10"
+          >
             <Image width={16} height={16} alt="Chain logo" src={getChainLogoSrc(chain.logo)} />
             <span>{chain.name}</span>
           </button>
         ))
       ) : (
-        <div>
+        <div className="px-middle py-small">
           <span>No data</span>
         </div>
       )}
