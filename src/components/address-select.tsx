bd637dc1a1511bf4188a28d3f6414fdde96cb05e
JayJay1024 <lygm1062@gmail.com>
2023-11-06 05:50:09 +0800

    Improve AddressSelect

--- src/components/address-select.tsx
+++ src/components/address-select.tsx
@@ -1,5 +1,6 @@
 import Select from "@/ui/select";
 import ConnectWallet from "./connect-wallet";
+import { WalletAccount } from "@talismn/connect-wallets";
 
 interface Value {
   name?: string;
@@ -8,31 +9,48 @@ interface Value {
 
 interface Props {
   value?: Value;
-  options: Value[];
+  options?: Value[];
+  accounts?: WalletAccount[];
   onChange?: (value: Value) => void;
+  onAccountChange?: (account: WalletAccount) => void;
 }
 
-export default function AddressSelect({ value, options, onChange = () => undefined }: Props) {
+export default function AddressSelect({
+  value,
+  options,
+  accounts,
+  onChange = () => undefined,
+  onAccountChange = () => undefined,
+}: Props) {
   return (
     <Select
-      label={value ? <span>{value.address}</span> : undefined}
-      placeholder={<span>Select an address</span>}
+      label={value ? <span className="truncate">{value.name || value.address}</span> : undefined}
+      placeholder={<span className="text-gray-400">Select an address</span>}
+      innerSuffix={<ConnectWallet />}
       sameWidth
-      childClassName="flex flex-col py-small"
+      labelClassName="flex items-center justify-between w-full pr-1 pl-middle py-1"
+      childClassName="flex flex-col py-middle bg-component border-primary border border-radius max-h-60 overflow-y-auto"
     >
       {options?.length ? (
-        options.map((account) => (
-          <button key={account.address} onClick={() => onChange(account)} className="flex items-center gap-small">
-            <span>{account.address}</span>
+        options.map((account, index) => (
+          <button
+            key={account.address}
+            onClick={() => {
+              onChange(account);
+              if (accounts?.at(index)) {
+                onAccountChange(accounts[index]);
+              }
+            }}
+            className="flex items-center gap-small px-middle py-1 transition-colors hover:bg-white/10"
+          >
+            <span className="truncate">{account.name || account.address}</span>
           </button>
         ))
       ) : (
-        <div>
+        <div className="px-middle py-small">
           <span>No data</span>
         </div>
       )}
-
-      {value ? null : <ConnectWallet />}
     </Select>
   );
 }
