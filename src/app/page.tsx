501ead6f70e6289f97a65f9bd98023d879b50ed7
JayJay1024 <lygm1062@gmail.com>
2023-11-06 05:54:54 +0800

    Page import Transfer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
+import Transfer from "@/components/transfer";
 import PageWrap from "@/components/page-wrap";
-import dynamic from "next/dynamic";
-
-const JayTest = dynamic(() => import("@/components/jay-test"), { ssr: false });
 
 export default function Home() {
   return (
     <PageWrap>
-      <JayTest />
+      <Transfer />
     </PageWrap>
   );
 }
