d1bfd03f506ac5e7679c12d9146eb25ac7a70b11
JayJay1024 <lygm1062@gmail.com>
2023-11-06 18:15:53 +0800

    Update text color of InputAlert

--- src/components/input-alert.tsx
+++ src/components/input-alert.tsx
@@ -1,7 +1,7 @@
 export function InputAlert({ text }: { text: string }) {
   return (
     <div className="absolute -bottom-5 left-0 inline-flex w-full">
-      <span className="text-xs font-light lowercase text-red-500">{text}</span>
+      <span className="text-xs font-light lowercase text-alert">{text}</span>
     </div>
   );
 }
