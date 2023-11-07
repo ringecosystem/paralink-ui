4743d7ce30fdd40f66d9aa22d5893d43d232cdce
JayJay1024 <lygm1062@gmail.com>
2023-11-07 15:03:32 +0800

    Update InputLabel space size

--- src/components/input-alert.tsx
+++ src/components/input-alert.tsx
@@ -1,6 +1,6 @@
 export function InputAlert({ text }: { text: string }) {
   return (
-    <div className="absolute -bottom-5 left-0 inline-flex w-full">
+    <div className="absolute -bottom-[1.2rem] left-0 inline-flex w-full">
       <span className="text-xs font-light lowercase text-alert">{text}</span>
     </div>
   );
