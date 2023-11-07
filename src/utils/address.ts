a0bc67ca9c4944c59c7c4a55bd10e1cc3080088b
JayJay1024 <lygm1062@gmail.com>
2023-11-07 18:57:23 +0800

    Utils toShortAddress

--- src/utils/address.ts
+++ src/utils/address.ts
@@ -4,6 +4,9 @@ import { hexToU8a, isHex } from "@polkadot/util";
 import { isAddress } from "viem";
 
 function isValidAddressPolkadotAddress(address: string) {
+  if (isAddress(address)) {
+    return false;
+  }
   try {
     encodeAddress(isHex(address) ? hexToU8a(address) : decodeAddress(address));
     return true;
@@ -14,4 +17,8 @@ function isValidAddressPolkadotAddress(address: string) {
 
 export function isValidAddress(address: string, addressType: AddressType) {
   return addressType === "evm" ? isAddress(address) : isValidAddressPolkadotAddress(address);
+}
+
+export function toShortAdrress(address: string) {
+  return address.length > 16 ? `${address.slice(0, 5)}...${address.slice(-4)}` : address;
 }
