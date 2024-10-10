760a1d73c9f479942054734ab3fe7afc16a5c4ba
JayJay1024 <lygm1062@gmail.com>
2024-10-10 10:05:12 +0800

    fix: chain name display

--- src/components/chainSelectInput.tsx
+++ src/components/chainSelectInput.tsx
@@ -81,32 +81,22 @@ export default function ChainSelectInput({ who, options }: { who: string; option
           height={20}
           alt={who === "sender" ? sourceChain.name : targetChain.name}
         />
-        <p className="text-[12px] leading-[15px]">
-          {who === "sender"
-            ? sourceChain.name.includes("Polkadot")
-              ? sourceChain.name.replace(" AssetHub", "")
-              : sourceChain.name
-            : targetChain.name.includes("Polkadot")
-            ? targetChain.name.replace(" AssetHub", "")
-            : targetChain.name}
-        </p>
+        <p className="text-[12px] leading-[15px]">{who === "sender" ? sourceChain.name : targetChain.name}</p>
         <span className="block h-[16px] w-[16px] bg-[url('/images/icons/downarrow-icon.svg')] bg-contain bg-center bg-no-repeat" />
       </div>
       <div
-        className="absolute left-0 right-0 top-[calc(100%+10px)] z-[300]  overflow-hidden rounded-[10px] shadow-xl duration-500"
+        className="absolute left-0 right-0 top-[calc(100%+10px)] z-[300]  overflow-hidden rounded-[10px] bg-white shadow-xl duration-500"
         style={{ maxHeight: open ? "10vh" : "0" }}
       >
-        <div className="relative flex h-fit w-fit  flex-col gap-[10px] rounded-[10px] bg-white p-[10px]">
+        <div className="relative flex h-fit w-full  flex-col gap-[10px] rounded-[10px] p-[10px]">
           {options.map((item) => (
             <div
               key={item.name}
-              className="flex w-fit items-center gap-[5px]"
+              className="flex w-full items-center gap-[5px]"
               onClick={who === "sender" ? () => _setSourceChain(item) : () => _setTargetChain(item)}
             >
               <Image src={getChainLogoSrc(item.logo)} width={20} height={20} alt={item.name} />
-              <p className="whitespace-nowrap text-[12px] leading-[15px]">
-                {item.name.includes("Polkadot") ? item.name.replace(" AssetHub", "") : item.name}
-              </p>
+              <p className="truncate whitespace-nowrap text-[12px] leading-[15px]">{item.name}</p>
             </div>
           ))}
         </div>
